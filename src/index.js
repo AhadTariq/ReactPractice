aff306bd5ff2c1d2b79e16369fc2ee15b603f91f
Ahad Tariq <CaptainXAhad@outlook.com>
2020-10-04 16:18:12 +0500

    Inheritance and modularity

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
-//Spreading
-const f = [1, 2, 4];
-const d = [3];
+import { Teacher } from "./teacher";
 
-const combined = [...f, ...d];
-console.log(combined);
-
-//class
-
-class Person {
-  contructor(name) {
-    this.name = name;
-  }
-
-  walk() {
-    console.log("Walk");
-  }
-}
-
-const person = new Person("Mosh");
-person.walk();
+const teacher = new Teacher("Ahad", "BSSE");
+teacher.teach();
