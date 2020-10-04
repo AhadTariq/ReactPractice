d045b08ee30af3af7981b20e0a3515b5b5bfe5f8
Ahad Tariq <CaptainXAhad@outlook.com>
2020-10-04 14:49:53 +0500

    Arrow Function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//const sq = (num) => num * num;
+//console.log(sq(5));
 
-const element = <h1>Hello World</h1>
-ReactDOM.render(element, document.getElementById('root'));
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: false },
+];
+
+//const activeJob = jobs.filter(function(job){return job.isActive;});
+
+const activeJob = jobs.filter((jobs) => jobs.isActive);
+console.log(activeJob);
